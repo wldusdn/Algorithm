444f1133a797cb96151d5a6ad44ad98a77078c1e
wldusdn <110551002+wldusdn@users.noreply.github.com>
2025-08-19 22:15:41 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 188 ms, Memory: 18312 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] chess = { 1, 1, 2, 2, 2, 8 };
+		for(int i=0 ;i < chess.length ;i++) {
+			chess[i] -= sc.nextInt();
+		}
+		for(int i=0 ;i < chess.length ;i++) {
+			System.out.print(chess[i]+" ");
+		}
+		
+	}
+}
