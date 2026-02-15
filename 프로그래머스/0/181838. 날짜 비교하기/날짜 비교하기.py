530a0c8d51a6c5c03d9365dc6a07fa032f3ea317
wldusdn <110551002+wldusdn@users.noreply.github.com>
2026-02-15 20:37:08 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,3 @@
+def solution(date1, date2):
+    if date1[0] < date2[0] or (date1[0] == date2[0] and date1[1] < date2[1]) or (date1[0] == date2[0] and date1[1] == date2[1] and date1[2] < date2[2]): return 1
+    return 0
