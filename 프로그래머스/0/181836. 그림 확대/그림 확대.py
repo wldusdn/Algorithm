1988e5939eaae22479e6284cb6e051ffeb7d0164
wldusdn <110551002+wldusdn@users.noreply.github.com>
2026-02-15 20:55:27 +0900

    [level 0] Title: 그림 확대, Time: 0.17 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -1,10 +1,13 @@
 def solution(picture, k):
     answer = []
-    for pic in picture:
-        word = ""
-        for p in pic:
-            for i in range(k):
-                word += p
-        for i in range(k):
-            answer.append(word)
+    # for pic in picture:
+    #     word = ""
+    #     for p in pic:
+    #         for i in range(k):
+    #             word += p
+    #     for i in range(k):
+    #         answer.append(word)
+    for i in range(len(picture)):
+        for _ in range(k):
+            answer.append(picture[i].replace(".", "."*k).replace("x","x"*k))
     return answer
